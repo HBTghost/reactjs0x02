5930769b4e1e4ed2c569a5ae7278dd13257a4476
HBTghost <hacbangtam@gmail.com>
2021-01-24 16:02:07 +0700

    init

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,22 @@
+import { Card, CardContent, Typography, Grid } from '@material-ui/core'
+import styles from './Cards.module.css'
+
+const Cards = ({ data }) => {
+  console.log(data)
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Infected</Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of active cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default Cards
